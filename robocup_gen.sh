2c5b45623fd754547453617858dcbbab29b20086
Yannis Konstas <sinantie@gmail.com>
2011-03-01 11:45:10 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@413 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- robocup_gen.sh
+++ robocup_gen.sh
@@ -3,4 +3,4 @@ java -Xmx1800m -cp dist/Generator.jar:dist/lib/kylm.jar:dist/lib/meteor.jar:dist
 -Djava.library.path=lib/wrappers induction.Generation -outputFullPred -create -modelType generate \
 -testInputLists $1 -inputFileExt events -execPoolDir $2 -stagedParamsFile ${3}/stage1.params.obj \
 -disallowConsecutiveRepeatFields -kBest $4 -ngramModelFile $5 -ngramWrapper kylm \
--outputExampleFreq 100  -averageTextLength $6 -ngramSize $7 -allowConsecutiveEvents -useGoldStandardOnly
+-outputExampleFreq 100  -averageTextLength $6 -ngramSize 2 -allowConsecutiveEvents -useGoldStandardOnly
