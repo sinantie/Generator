d80ab2d256655091dc3736fa5a6ba154e45bd05d
Yannis Konstas <sinantie@gmail.com>
2011-03-17 19:32:50 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@454 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- test/induction/SemParseWeatherTest.java
+++ test/induction/SemParseWeatherTest.java
@@ -38,18 +38,18 @@ public class SemParseWeatherTest
          String args = "-modelType semParse -testInputLists test/testWeatherGovEvents "
 //         String args = "-modelType semParse -testInputLists gaborLists/semParseWeatherEval200 "
                     + "-inputFileExt events -stagedParamsFile "
-                    + "results/output/weatherGov/alignments/model_3_gabor_dev_some_times"
+                    + "results/output/weatherGov/alignments/model_3_gabor_mapVecs"
                     + "/0.exec/stage1.params.obj "
-                    + "-disallowConsecutiveRepeatFields -kBest 10 -ngramSize 3 "
-                    + "-ngramModelFile weatherGovLM/gabor-srilm-abs-weather-semantic-dev-some-times-noisy-3-gram.model.arpa "
+                    + "-disallowConsecutiveRepeatFields -kBest 10 -ngramSize 2 "
+                    + "-ngramModelFile weatherGovLM/gabor-srilm-abs-weather-semantic-3-gram.model.arpa "
                     + "-ngramWrapper srilm -reorderType eventTypeAndField "
                     + "-maxPhraseLength 5 -newFieldPerWord 0,-1 "
-                    + "-modelUnkWord -allowConsecutiveEvents -allowNoneEvent "
-
-                    + " -excludedFields temperature.time windChill.time "
-                    + "windSpeed.time windDir.time gust.time "
-                    + "precipPotential.time thunderChance.time "
-                    + "snowChance.time freezingRainChance.time sleetChance.time";
+                    + "-modelUnkWord -allowConsecutiveEvents";
+//
+//                    + " -excludedFields temperature.time windChill.time "
+//                    + "windSpeed.time windDir.time gust.time "
+//                    + "precipPotential.time thunderChance.time "
+//                    + "snowChance.time freezingRainChance.time sleetChance.time";
         /*initialisation procedure from Generation class*/
         Options opts = new Options();
         Execution.init(args.split(" "), new Object[] {opts}); // parse input params
