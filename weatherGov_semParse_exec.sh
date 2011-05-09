050848844597d6068d2007b9603231f5066bcfd2
Yannis Konstas <sinantie@gmail.com>
2011-05-09 09:59:50 +0000

    adding bikes-related lists and LM
    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@532 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- weatherGov_semParse_exec.sh
+++ weatherGov_semParse_exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Fold 1
-./weatherGov_semParse.sh gaborLists/semParseWeatherEval200 results/output/weatherGov/semParse/semParse_no_n_no_t_LM_3_all \
-results/output/weatherGov/alignments/model_3_gabor_mapVecs/0.exec 10 \
-weatherGovLM/gabor-srilm-abs-weather-semantic-full-no-times-no-none-3-gram.model.arpa
+./weatherGov_semParse.sh test/testWeatherGovEvents results/output/weatherGov/semParse/semParse_full_some_times \
+results/output/weatherGov/alignments/model_3_gabor_mapVecs_some_times/0.exec 10 \
+weatherGovLM/gabor-srilm-abs-weather-semantic-full-some-times-3-gram.model.arpa
 
