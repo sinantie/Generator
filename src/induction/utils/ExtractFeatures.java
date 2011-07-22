0e8a30e7229fcd6182586f08ccb0494dafcb5860
Yannis Konstas <sinantie@gmail.com>
2011-07-22 17:48:50 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@651 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- src/induction/utils/ExtractFeatures.java
+++ src/induction/utils/ExtractFeatures.java
@@ -3,6 +3,7 @@ package induction.utils;
 import induction.Utils;
 import induction.problem.event3.Event3Model;
 import induction.problem.event3.EventType;
+import induction.problem.event3.Field;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -24,9 +25,9 @@ import java.util.Map;
  */
 public class ExtractFeatures
 {
-    private String outputFilename, inputFilename;
-    private Map<String, EventType> eventTypesMap;
-    private String[] emptyVector;
+    private String outputFilename, inputFilename, header;
+    private Map<String, Integer> eventTypesIndex;
+    private String[] emptyVector;    
     public enum FeatureType {BINARY, COUNTS, VALUES};
     private FeatureType type;
 
@@ -35,13 +36,13 @@ public class ExtractFeatures
     {
         this.outputFilename = outputFilename;
         this.inputFilename = inputFilename;
-        loadEventTypes(paramsFilename);
-        emptyVector = createEmptyVector();
         this.type = type;
+        emptyVector = createEmptyVector(loadEventTypes(paramsFilename));
     }
 
-    private void loadEventTypes(String paramsFilename)
+    private int loadEventTypes(String paramsFilename)
     {
+        int totalNumberOfFields = 0;
         try
         {
             ObjectInputStream ois = new ObjectInputStream(
@@ -49,15 +50,32 @@ public class ExtractFeatures
             ois.readObject(); // wordIndexer, don't need it
             ois.readObject(); // labelIndexer, don't need it
             EventType[] eventTypes = (EventType[]) ois.readObject(); // we only need this one
-            eventTypesMap = new HashMap<String, EventType>();
+            eventTypesIndex = new HashMap<String, Integer>();
+            StringBuilder str = new StringBuilder(); // create header
             for(EventType eventType: eventTypes)
-                eventTypesMap.put(eventType.getName(), eventType);
+            {
+                String eventName = eventType.getName();
+                for(int i = 0; i < eventType.getF(); i++)
+                    str.append(eventName).append("_").append(eventType.fieldToString(i)).append(",");
+//                    str.append(eventName).append(",");
+                    
+                // compute total number of elements: ~|eventTypes|*|fields_per_eventType|
+                eventTypesIndex.put(eventName, totalNumberOfFields);
+                totalNumberOfFields += eventType.getF();
+//                totalNumberOfFields++;
+            }
+            str.append("text_length").append("\n");
+            header = str.toString();
             ois.close();
         }
         catch(Exception ioe)
         {
             Utils.log("Error loading "+ paramsFilename);
             ioe.printStackTrace();
+        }
+        finally
+        {
+            return totalNumberOfFields;
         }
     }
 
@@ -66,17 +84,18 @@ public class ExtractFeatures
         try
         {
             FileWriter fos = new FileWriter(outputFilename);
-            fos.append(header());
+            fos.append(header);
             String key = null;
+            String example[];
             StringBuilder str = new StringBuilder();
             for(String line : Utils.readLines(inputFilename))
             {
                 if(line.startsWith("Example_"))
                 {
                     if(key != null) // only for the first example
-                    {
-                        // parse events part only from example and extract features from it
-                        fos.append(extractFeatures(Event3Model.extractExampleFromString(str.toString())[2]));
+                    {                        
+                        example = Event3Model.extractExampleFromString(str.toString());
+                        fos.append(extractFeatures(example[1], example[2]) + "\n");
                         str = new StringBuilder();
                     }
                     key = line;
@@ -84,49 +103,52 @@ public class ExtractFeatures
                 str.append(line).append("\n");
             }  // for
             // don't forget last example
-            fos.append(extractFeatures(Event3Model.extractExampleFromString(str.toString())[2]));
+            example = Event3Model.extractExampleFromString(str.toString());
+            fos.append(extractFeatures(example[1], example[2]));
+            fos.close();
         }
         catch(IOException ioe) {}
     }
-
-    /**
-     * Extract features from events String and append them in csv format.
-     * Each line of the feature vector corresponds to the fields of all the eventTypes.
-     * Each element of the vector may take (1) a binary value if the particular field
-     * of the event has a value other than '--' at least once. A variant (2) is
-     * to put the number of times the particular field has occured in the dataset,
-     * as a single eventType may be accounted in the input more than once.
-     * Another option is (3) to put the value of the particular field (this cannot
-     * take into account datasets which have more than one events of the same eventType)
-     *
-     * @param events
-     * @return
-     */
-    private String extractFeatures(String events)
+    
+    private String extractFeatures(String text, String events)
     {
-//        StringBuilder str = new StringBuilder();
         String[] vector = Arrays.copyOf(emptyVector, emptyVector.length);
         for(String line : events.split("\n"))
         {
             fillVector(vector, line);
         }
-        return Arrays.toString(vector);
-        //return str.toString();
+        // put the text length as label
+        return Arrays.toString(vector).replaceAll("[\\[\\] ]", "") + "," + text.split(" ").length;
     }
 
     private void fillVector(String[] vector, String event)
     {
+        String[] tokens = event.split("\t");
+        int index = eventTypesIndex.get(tokens[1].split(":")[1]); // 2nd token holds the type (.type:xxx)
+//        vector[index] = type == FeatureType.BINARY ? "1" : String.valueOf(Integer.valueOf(vector[index]) + 1);;
 
+        for(int i = 2; i < tokens.length; i++)
+        {
+            int currentIndex = index + i - 2;
+            String value = tokens[i].split(":")[1];
+            if(!value.equals("--"))
+            {
+                if(type == FeatureType.BINARY)
+                    vector[currentIndex] = "1";
+                else if(type == FeatureType.COUNTS)
+                    vector[currentIndex] = String.valueOf(Integer.valueOf(vector[currentIndex]) + 1);
+                else if(type == FeatureType.VALUES)
+                    // account only for the first event of this eventType
+                    if(vector[currentIndex].equals("--"))
+                    {
+                        vector[currentIndex] = value;
+                    }
+            } // if
+        } // for
     }
 
-    private String[] createEmptyVector()
-    {
-        // compute total number of elements: ~|eventTypes|*|fields_per_eventType|
-        int numberOfElements = 0;
-        for(EventType eventType : eventTypesMap.values())
-        {
-            numberOfElements += eventType.getF();
-        }
+    private String[] createEmptyVector(int numberOfElements)
+    {        
         String[] out = new String[numberOfElements];
         // Variant (1) - binary values
         if(type==FeatureType.BINARY || type==FeatureType.COUNTS)
@@ -140,19 +162,21 @@ public class ExtractFeatures
         return out;
 
     }
-    private String header()
+
+    private String header(EventType[] eventTypes)
     {
         return "";
     }
-
+  
     public static void main(String[] args)
     {
         String paramsFilename = "results/output/atis/alignments/model_3/"
                 + "15_iter_no_null_no_smooth_STOP/stage1.params.obj";
-        String outputFilename = "data/atis/train/atis5000.sents.full.features";
-        String inputFilename = "data/atis/train/atis5000.sents.full";
-        FeatureType type = FeatureType.BINARY;
-//        String inputFilename = "../atis/lambda/percy/test/atis-test.txt";
+//        String outputFilename = "data/atis/train/atis5000.sents.full.counts.features.csv";
+//        String inputFilename = "data/atis/train/atis5000.sents.full";
+        String outputFilename = "data/atis/test/atis-test.txt.counts.features.csv";
+        String inputFilename = "data/atis/test/atis-test.txt";
+        FeatureType type = FeatureType.COUNTS;
         ExtractFeatures ef = new ExtractFeatures(outputFilename, inputFilename, paramsFilename, type);
         ef.execute();
     }
