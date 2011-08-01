b921c7219b988b02d1d6cd044dddc2cabdc8f5d9
Yannis Konstas <sinantie@gmail.com>
2011-08-01 10:56:06 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@663 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- atis_gen.sh
+++ atis_gen.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-./java_run_gen.sh data/atis/test/atis-test.txt \
-results/output/atis/generation/model_3_40-best_no_null_no_smooth_STOP_reduced_events \
-results/output/atis/alignments/model_3/15_iter_no_null_no_smooth_STOP_reduced_events 40 \
-atisLM/atis-all-train-3-gram.model.arpa srilm -examplesInSingleFile
+java -Xmx4g -cp dist/Generator.jar:dist/lib/Helper.jar:dist/lib/kylm.jar:dist/lib/meteor.jar:dist/lib/tercom.jar:dist/lib/srilmWrapper:\
+dist/stanford-postagger-2010-05-26.jar \
+-Djava.library.path=lib/wrappers induction.Generation \
+-outputFullPred -create \
+-examplesInSingleFile \
+-modelType generate \
+-inputFileExt events \
+-disallowConsecutiveRepeatFields \
+-ngramWrapper srilm \
+-outputExampleFreq 100 \
+-allowConsecutiveEvents \
+-reorderType eventType \
+-maxPhraseLength 5 \
+-binariseAtWordLevel \
+-kBest 40 \
+-testInputLists data/atis/test/atis-test.txt \
+-execDir results/output/atis/generation/model_3_40-best_no_null_no_smooth_STOP_predLength_0 \
+-stagedParamsFile results/output/atis/alignments/model_3/15_iter_no_null_no_smooth_STOP/stage1.params.obj \
+-ngramModelFile atisLM/atis-all-train-3-gram.model.arpa \
+-lengthPredictionModelFile data/atis/train/lengthPrediction.counts.linear-reg.model \
+-lengthPredictionFeatureType COUNTS \
+-lengthPredictionStartIndex 2 \
+-lengthCompensation 0
+#-excludedEventTypes airline airport booking_class city entity fare_basis_code location transport
+#-excludedFields flight.stop
