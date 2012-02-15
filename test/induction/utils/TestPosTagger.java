afb6e3a27ce31c23ab0dbd127a614702d5ba0915
Yannis Konstas <sinantie@gmail.com>
2012-02-15 18:39:17 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@885 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- test/induction/utils/TestPosTagger.java
+++ test/induction/utils/TestPosTagger.java
@@ -12,17 +12,19 @@ public class TestPosTagger extends TestCase
 
     public TestPosTagger(String name)
     {
-        super(name);
-        posTagger = new PosTagger();
+        super(name);        
     }
 
     public void testPosTagger()
     {        
-        assertEquals("./.", posTagger.tag(".").trim());
-        assertEquals(",/,", posTagger.tag(",").trim());
-        assertEquals("-LRB-/-LRB-", posTagger.tag("(").trim());
-        assertEquals("-RRB-/-RRB-", posTagger.tag(")").trim());
-        assertEquals("--/:", posTagger.tag("--").trim());
-        assertEquals("and/CC", posTagger.tag("and").trim());
+//        assertEquals("./.", posTagger.tag(".").trim());
+//        assertEquals(",/,", posTagger.tag(",").trim());
+//        assertEquals("-LRB-/-LRB-", posTagger.tag("(").trim());
+//        assertEquals("-RRB-/-RRB-", posTagger.tag(")").trim());
+//        assertEquals("--/:", posTagger.tag("--").trim());
+//        assertEquals("and/CC", posTagger.tag("and").trim());
+        
+        posTagger = new PosTagger("data/atis/train/atis5000.sents.full", "file_events");
+        posTagger.execute();
     }
 }
