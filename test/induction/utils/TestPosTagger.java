19c6b99f0a85715b6455632328019ca3531833a3
Yannis Konstas <sinantie@gmail.com>
2012-02-24 19:49:32 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@905 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- test/induction/utils/TestPosTagger.java
+++ test/induction/utils/TestPosTagger.java
@@ -24,7 +24,12 @@ public class TestPosTagger extends TestCase
 //        assertEquals("--/:", posTagger.tag("--").trim());
 //        assertEquals("and/CC", posTagger.tag("and").trim());
         
-        posTagger = new PosTagger("data/atis/train/atis5000.sents.full", "file_events", "useUniversalTags");
+        posTagger = new PosTagger("data/atis/train/atis5000.sents.full", 
+                                  PosTagger.TypeOfPath.file, 
+                                  PosTagger.TypeOfInput.events, 
+                                 "/home/konstas/EDI/candc/candc-1.00/atis_tagged_manual_disambiguated.out_sorted",
+                                 false,
+                                 "");
         posTagger.execute();
     }
 }
