ef0e41a5711931aec1fd9fdb08f162141e352238
Yannis Konstas <sinantie@gmail.com>
2012-03-14 17:48:29 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@936 554d2c04-29e4-4109-9b11-0fd97a6c5eca

---
+++ test/induction/runtime/DMVTrainWeatherGovTest.java
@@ -0,0 +1,61 @@
+package induction.runtime;
+
+import fig.exec.Execution;
+import induction.LearnOptions;
+import induction.Options;
+import induction.Options.InitType;
+import induction.problem.dmv.generative.GenerativeDMVModel;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author konstas
+ */
+public class DMVTrainWeatherGovTest
+{
+    LearnOptions lopts;
+    String name;
+    GenerativeDMVModel model;
+    
+    @Before
+    public void setUp() 
+    {        
+         String args = "-modelType dmv "
+                    + "-Options.stage1.numIters 30 "
+                    + "-numThreads 2 "                                        
+                    + "-inputLists "
+                    + "gaborLists/trainListPathsGabor "
+                    + "-inputFileExt events "
+                    + "-inputFormat raw "
+                    + "-dontOutputParams "
+                    + "-useTagsAsWords "
+                    + "-posAtSurfaceLevel "
+                    + "-initSmoothing 0.01 "
+                    + "-initNoise 1e-3";                    
+//                    + "-usePosTagger "            
+
+         
+        /*initialisation procedure from Induction class*/
+        Options opts = new Options();
+        Execution.init(args.split(" "), new Object[] {opts}); // parse input params        
+        model = new GenerativeDMVModel(opts);
+        model.readExamples();
+        model.logStats();
+        model.preInit();
+        model.init(InitType.uniformz, opts.initRandom, "stage1");
+        opts.outputIterFreq = opts.stage1.numIters;
+        lopts = opts.stage1;
+        name = "stage1";
+    }
+   
+    /**
+     * Test of run method, of class Induction.
+     */
+    @Test
+    public void testRun()
+    {
+        System.out.println("run");        
+        System.out.println(model.testInitLearn(name, lopts));
+    }
+}
