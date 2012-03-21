de34673398142ad0ee623ab4e0271c48b38bc790
Yannis Konstas <sinantie@gmail.com>
2012-03-21 16:23:18 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@949 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- src/induction/utils/ProcessGridSearchFiles.java
+++ src/induction/utils/ProcessGridSearchFiles.java
@@ -22,13 +22,16 @@ public class ProcessGridSearchFiles
     String[] formattedStringFragments;
     enum Type {GENERATION, ALIGN};
     Type type;
+    double[] weights;
+    boolean crossValidate;
     
-    private ProcessGridSearchFiles(String path, String formattedFolder, String outputFile, Type type)
+    private ProcessGridSearchFiles(String path, String formattedFolder, String outputFile, Type type, boolean crossValidate)
     {
         this.path = path;
         this.formattedFolder = formattedFolder;
         this.outputFile = outputFile;
         this.type = type;
+        this.crossValidate = crossValidate;
         blocksMap = new TreeMap<Double, BlockOfResults>();
         parseFormattedString();
     }
@@ -41,7 +44,11 @@ public class ProcessGridSearchFiles
             {
                 String[] res = parseDirName(f, posOfParams);
                 if(res != null)
-                    addResult(res[0], new Result(res));
+                {
+                    Result result = crossValidate ? new CrossResult(res) : new Result(res);
+                    result.parseResults();
+                    addResult(res[0], result);
+                }
             } // for
             saveOutputToFile();
         } // if
@@ -114,26 +121,49 @@ public class ProcessGridSearchFiles
                 posOfParams[1] = i;
         }               
     }
+
+    public void setWeights(double[] weights)
+    {
+        this.weights = weights;
+    }
     
     public static void main(String[] args)
     {
-        String path = "results/output/atis/generation/dependencies_uniformZ/grid/";
-        String formattedString = "model_3_$param1$-best_0.01_STOP_inter$param2$_condLM_hypRecomb_lmLEX_POS_predLength";
-        String outputFile = "results/output/atis/generation/dependencies_uniformZ/grid/grid.results";        
+//        String path = "results/output/atis/generation/dependencies_uniformZ/grid/";
+//        String formattedString = "model_3_$param1$-best_0.01_STOP_inter$param2$_condLM_hypRecomb_lmLEX_POS_predLength";
+//        String outputFile = "results/output/atis/generation/dependencies_uniformZ/grid/grid.results";        
+        String path = "results/output/robocup/generation/dependencies/grid";        
+        String formattedString = "model_3_$param1$-best_inter$param2$_new4";
+        String outputFile = "results/output/robocup/generation/dependencies/grid/grid.results";        
+        double[] weights = {513.0, 365.0, 214.0, 311.0};
+        boolean crossValidate = true;
         
-        if(args.length == 2)
-        {
-            path = args[0];            
-            outputFile = args[1];
-        }
         if(args.length == 3)
+        {
+            path = args[0];
+            outputFile = args[1];
+            crossValidate = Boolean.valueOf(args[2]);
+        }
+        if(args.length > 3)
         {
             path = args[0];
             formattedString = args[1];
             outputFile = args[2];
+            crossValidate = Boolean.valueOf(args[3]);
+        }
+        if(args.length == 5)
+        {
+            String[] ar = args[3].split("#");
+            weights = new double[ar.length];
+            for(int i = 0; i < ar.length; i++)
+            {
+                weights[i] = Double.valueOf(ar[i]);
+            }
         }
         Type type = Type.GENERATION;
-        ProcessGridSearchFiles pgsf = new ProcessGridSearchFiles(path, formattedString, outputFile, type);
+        ProcessGridSearchFiles pgsf = new ProcessGridSearchFiles(path, formattedString, outputFile, type, crossValidate);
+        if(weights != null)
+            pgsf.setWeights(weights);
         pgsf.execute();
     }
     
@@ -199,22 +229,13 @@ public class ProcessGridSearchFiles
     class Result implements Comparable<Result>
     {
         String param1, param2, dirName;
-        List<String> results;
+        List<Double> results;
 
         public Result(String param1, String param2, String dirName)
         {
             this.param1 = param1;
             this.param2 = param2;
-            this.dirName = dirName;
-            results = new ArrayList<String>();
-            try
-            {
-                parseResults(path + dirName + "/stage1.test.performance.results");
-            }
-            catch(Exception e)
-            {
-                System.err.println("Error in directory " + dirName);
-            }
+            this.dirName = dirName;                
         }
         
         public Result(String[] res)
@@ -222,8 +243,22 @@ public class ProcessGridSearchFiles
             this(res[0], res[1], res[2]);
         }
         
-        private void parseResults(String filename)
+        public void parseResults()
         {
+            try
+            {
+                results = parseResults(path + "/" + dirName);
+            }
+            catch(Exception e)
+            {
+                System.err.println("Error in directory " + dirName);
+            }
+        }
+        
+        protected List<Double> parseResults(String dirname)
+        {
+            String filename =  dirname + "/stage1.test.performance.results";
+            List<Double> res = new ArrayList<Double>();
             String lines[] = Utils.readLines(filename);
             if (type == Type.GENERATION)
             {
@@ -234,33 +269,35 @@ public class ProcessGridSearchFiles
                         foundBleu = true;
                     if(foundBleu && line.contains("Averaged Bleu scores:"))
                     {
-                        results.add(parseResult(line));
+                        res.add(parseResult(line));
                         foundBleu = false;
                     } // if BLEU
                     if(line.equals("METEOR scores"))
                         foundMeteor = true;
                     if(foundMeteor && line.contains("Final score:"))
                     {
-                        results.add(parseResult(line));
+                        res.add(parseResult(line));
                         foundMeteor = false;
                     } // if METEOR
                     if(line.contains("Total Recall:"))
-                        results.add(parseResult(line));
+                        res.add(parseResult(line));
                 } // for
             } // if
+            return res;
         }
         
-        private String parseResult(String in)
+        private Double parseResult(String in)
         {
-            return Utils.fmt(Double.valueOf(in.split(":")[1].trim()));
+//            return Utils.fmt(Double.valueOf(in.split(":")[1].trim()));
+            return Double.valueOf(in.split(":")[1].trim());
         }
-        
+         
         public String toString()
         {
             StringBuilder str = new StringBuilder();
             str.append(param1).append("\t").append(param2).append(":\t");
-            for(String res : results)
-                str.append(res).append("\t");
+            for(Double res : results)
+                str.append(Utils.fmt(res)).append("\t");
             return str.toString();
         }
 
@@ -269,6 +306,55 @@ public class ProcessGridSearchFiles
         {            
             return (Double.valueOf(param1) + Double.valueOf(param2)) -
                    (Double.valueOf(r.param1) + Double.valueOf(r.param2)) >= 0 ? 1 : -1; 
+        }
+    }
+    
+    class CrossResult extends Result 
+    {
+        double[] weights;
+        
+        public CrossResult(String[] res)
+        {
+            super(res);
+        }
+        
+        public CrossResult(String[] res, double[] weights)
+        {
+            this(res);
+            this.weights = weights;
+        }
+             
+        @Override
+        protected List<Double> parseResults(String dirname)
+        {
+            List<Double> res = new ArrayList<Double>();
+            int totalFolds = 0;
+            for(String foldDirname : new File(dirname).list())
+            {
+                if(foldDirname.matches("fold[0-9]+"))
+                {
+                    add(super.parseResults(dirName + "/" + foldDirname), res, 
+                            Integer.valueOf(foldDirname.substring(foldDirname.indexOf("fold") + 4)));
+                    totalFolds++;                    
+                }
+            } // for
+            double normaliser = weights == null ? (double) totalFolds : Utils.sum(weights);
+            for(int i = 0; i < res.size(); i++)
+            {
+                res.set(i, res.get(i) / normaliser);
+            }            
+            return res;
+        }
+        
+        private void add(List<Double> from, List<Double> to, int fold)
+        {
+            if(from.isEmpty()) // first result set
+                to.addAll(from);
+            else                
+                for(int i = 0; i < Utils.same(from.size(), to.size()); i++)
+                {
+                    to.set(i, to.get(i) + (weights != null ? weights[fold] * from.get(i) : from.get(i)));
+                }
         }
     }
 }
