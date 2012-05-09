be7a819956bd0a5ae334b047c5f5bcba00c9c808
Yannis Konstas <sinantie@gmail.com>
2012-05-09 20:47:04 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@967 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- process_grid.sh
+++ process_grid.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#RoboCup
+#input=results/output/robocup/generation/dependencies/NO_POS/
+#formattedString=model_3_\$param1\$-best_inter\$param2\$_new4_full_joint
+#output=results/output/robocup/generation/dependencies/NO_POS/grid.weighted.full_joint.results
+#crossValidate=true
+#weights=513#365#214#311
+
 #WeatherGov
 #input=results/output/weatherGov/generation/dependencies/
 #formattedString=model_3_\$param1\$-best_0.01_STOP_inter\$param2\$_condLM_hypRecomb_lmLEX_POS_predLength
@@ -7,12 +14,12 @@
 #crossValidate=false
 #COMMENT weights parameter
 
-#RoboCup
-input=results/output/robocup/generation/dependencies/NO_POS/
-formattedString=model_3_\$param1\$-best_inter\$param2\$_new4_full_joint
-output=results/output/robocup/generation/dependencies/NO_POS/grid.weighted.full_joint.results
-crossValidate=true
-weights=513#365#214#311
+#Atis
+input=results/output/atis/generation/dependencies_uniformZ_all/
+formattedString=model_3_\$param1\$-best_0.01_STOP_inter\$param2\$_condLM_hypRecomb_lmLEX_POS_predLength
+output=results/output/atis/generation/dependencies_uniformZ_all/grid.results
+crossValidate=false
+#COMMENT weights parameter
 
 java -cp dist/Generator.jar induction.utils.ProcessGridSearchFiles \
 ${input} \
