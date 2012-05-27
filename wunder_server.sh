a12d4770b786da6bd64459a399ffd401f8bd5291
Yannis Konstas <sinantie@gmail.com>
2012-05-27 19:05:36 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@996 554d2c04-29e4-4109-9b11-0fd97a6c5eca

---
+++ wunder_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+format=wunderground
+port=4444
+
+java -Xmx2g -cp dist/Generator.jar:dist/lib/Helper.jar:dist/lib/kylm.jar:dist/lib/meteor.jar:dist/lib/tercom.jar:dist/lib/srilmWrapper:\
+dist/stanford-postagger-2010-05-26.jar \
+-Djava.library.path=lib/wrappers induction.runtime.server.MultiServer $format\
+$port
+
