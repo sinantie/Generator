50ae8be05243ba22251469f448d9e7a2c26f1d8f
Yannis Konstas <sinantie@gmail.com>
2012-10-16 17:36:38 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@1128 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- weatherGov_align_model.sh
+++ weatherGov_align_model.sh
@@ -1,41 +1,69 @@
 #!/bin/bash
 threads=2
-#genDevListPathsGabor, trainListPathsGabor, genEvalListPathsGabor
-input=gaborLists/genDevListPathsGabor
-output=results/output/weatherGov/alignments/dev/model_3_gabor_cond_null_bigrams_viterbiEM_uniformz
+#gaborLists/genDevListPathsGabor, trainListPathsGabor, genEvalListPathsGabor
+#data/weatherGov/weatherGovGenDevGaborRecordTreebank.gz, weatherGovTrainGaborRecordTreebank.gz
+input=data/weatherGov/weatherGovTrainGaborRecordTreebank.gz
+output=results/output/weatherGov/alignments/pcfg/model_3_gabor_record_pcfg_treebank
+treebankRules=data/weatherGov/treebanks/recordTreebankRulesTrainRightBinarize
 memory=-Xmx2g
 java $memory -cp dist/Generator.jar:dist/lib/Helper.jar:dist/lib/kylm.jar:dist/lib/meteor.jar:dist/lib/tercom.jar:\dist/lib/srilmWrapper:\
 dist/stanford-postagger-2010-05-26.jar -ea -Djava.library.path=lib/wrappers induction.runtime.Induction \
 -create \
--modeltype event3 \
+-modeltype event3pcfg \
+-examplesInSingleFile \
 -inputLists $input \
 -execDir $output \
 -Options.stage1.numIters 15 \
 -inputFileExt events \
 -numThreads $threads \
--indepEventTypes 0,10 \
+-treebankRules $treebankRules \
+-fixRecordSelection \
+-initType artificial \
 -indepFields 0,5 \
 -indepWords 0,5 \
 -newEventTypeFieldPerWord 0,5 \
 -newFieldPerWord 0,5 \
 -disallowConsecutiveRepeatFields \
--dontCrossPunctuation \
--Options.stage1.smoothing 0.1 \
 -noneFieldSmoothing 0 \
 -outputFullPred \
 -modelUnkWord \
 -outputExampleFreq 500 \
--conditionNoneEvent \
--allowNoneEvent \
--initType uniformz \
--initNoise 0 \
--initSmoothing 0.01 \
 -initNoise 1e-3 \
--Options.stage1.hardUpdate
+-initSmoothing 0.01
+
+# Record PCFG - Treebank Input
+#-treebankRules $treebankRules \
+#-fixRecordSelection \
+#-initType artificial \
+#-indepFields 0,5 \
+#-indepWords 0,5 \
+#-newEventTypeFieldPerWord 0,5 \
+#-newFieldPerWord 0,5 \
+
+# Record HMM alignment
+# --------------------
+#-modeltype event3 \
+#-Options.stage1.numIters 15 \
+#-indepEventTypes 0,10 \
+#-indepFields 0,5 \
+#-indepWords 0,5 \
+#-newEventTypeFieldPerWord 0,5 \
+#-dontCrossPunctuation \
+#-conditionNoneEvent \
+#-allowNoneEvent \
+
+
+# Viterbi EM params
+# -----------------
+#-Options.stage1.smoothing 0.1 \
+#-initType uniformz \
+#-initNoise 0
+#-Options.stage1.hardUpdate
 
 #-inputPosTagged
 
-
+# Misc
+# ----
 #-useStopNode \
 #-useGoldStandardOnly
 #-excludedEventTypes airline airport booking_class city entity fare_basis_code location transport
