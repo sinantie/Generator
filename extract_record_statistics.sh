223d2118e9df5ec5d0dd690019da972ea3c288c9
Yannis Konstas <sinantie@gmail.com>
2012-10-22 18:32:16 +0000

    git-svn-id: https://svn.ecdf.ed.ac.uk/repo/inf/konstas/Generator@1143 554d2c04-29e4-4109-9b11-0fd97a6c5eca

--- extract_record_statistics.sh
+++ extract_record_statistics.sh
@@ -3,11 +3,11 @@
 #genDevListPathsGabor, trainListPathsGabor, genEvalListPathsGabor
 #inputLists=gaborLists/genDevListPathsGabor
 #data/weatherGov/weatherGovGenDevGaborRecordTreebank.gz, weatherGovTrainGaborRecordTreebank.gz, weatherGovGenEvalGaborRecordTreebank
-input=data/weatherGov/weatherGovTrainGaborRecordTreebank.gz
+input=data/weatherGov/weatherGovGenDevGaborRecordTreebank.gz
 #stagedParamsFile=results/output/weatherGov/alignments/pos/model_3_cond_null_POS_CDNumbers/stage1.params.obj.gz
 stagedParamsFile=results/output/weatherGov/alignments/model_3_gabor/1.exec/stage1.params.obj
 
-execDir=data/weatherGov/treebanks/standardBinarization
+execDir=data/weatherGov/treebanks
 java -Xmx1g -cp dist/Generator.jar:dist/lib/Helper.jar:dist/lib/kylm.jar:dist/lib/meteor.jar:dist/lib/tercom.jar:dist/lib/srilmWrapper:\
 dist/stanford-postagger-2010-05-26.jar \
 -Djava.library.path=lib/wrappers induction.utils.ExtractRecordsStatisticsExecutor \
@@ -25,7 +25,9 @@ dist/stanford-postagger-2010-05-26.jar \
 -delimitSentences \
 -extractRecordTrees \
 -binarize right \
--useEventTypeNames
+-useEventTypeNames \
+-markovOrder 2 \
+-modifiedBinarization
 
 #-modifiedBinarization
 #-useEventTypeNames
