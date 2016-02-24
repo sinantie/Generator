ba28d2f7f78b762efb9a146bbe960c7365c67366
Yonatan Bisk <ybisk@isi.edu>
2016-02-24 15:10:46 -0800

    change gen params

--- scripts/atis_gen.sh
+++ scripts/atis_gen.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-inputLists=../datasets/atis/test/atis-test.txt
+inputLists=datasets/atis-test.txt
 numThreads=4
-stagedParamsFile=results/output/atis/alignments/model_3/prior_0.01_POS/stage1.params.obj.gz
-dmvModelParamsFile=results/output/atis/dmv/train/atis_raw5000_full_indexers_001_POS_50/stage1.dmv.params.obj.gz
+stagedParamsFile=results/atis/alignments/model_3/0.exec/stage1.params.obj.gz
+dmvModelParamsFile=results/atis/dmv/train/atis_raw5000_full_indexers_001_POS_50/stage1.dmv.params.obj.gz
 kBest=120
 interpolationFactor=0.3
-execDir=results/output/atis/generation/dependencies/noRecursiveWeight/model_3_${kBest}-best_0.01_STOP_inter${interpolationFactor}_condLM_hypRecomb_lmLEX_POS_predLength
+execDir=results/atis/generation/
 
 CUR_DIR=`pwd`
 cd ..
-java -Xmx10000m -cp dist/Generator.jar:lib/Helper.jar:lib/kylm.jar:lib/meteor.jar:lib/tercom.jar:lib/srilmWrapper:\
-stanford-postagger-2010-05-26.jar \
--Djava.library.path=lib/wrappers induction.runtime.Generation \
+
+java -Xmx16000m -cp lib/jung/collections-generic-4.01.jar:lib/commons-math-2.2.jar:lib/jung/colt-1.2.0.jar:lib/jung/concurrent-1.3.4.jar:lib/jackson-annotations-2.0.2.jar:lib/jackson-core-2.0.2.jar:lib/jackson-databind-2.0.2.jar:lib/jung/jung-algorithms-2.0.1.jar:lib/jung/jung-graph-impl-2.0.1.jar:lib/jung/jung-hypergraph-visualization-1.0.jar:lib/jung/jung-api-2.0.1.jar:lib/jung/jung-io-2.0.1.jar:lib/jung/jung-jai-2.0.1.jar:lib/jung/jung-visualization-2.0.1.jar:lib/stanford-corenlp-3.5.1-models.jar:lib/stanford-corenlp-3.5.1.jar:lib/jung/stax-api-1.0.1.jar:lib/jung/vecmath-1.3.1.jar:lib/weka.jar:lib/jung/wstx-asl-3.2.6.jar:lib/kylm.jar:lib/Helper.jar:lib/meteor.jar:lib/srilmWrapper.jar:lib/tercom.jar:lib/RoarkWrapper.jar:dist/Generator.jar \
+induction.runtime.Generation \
 -numThreads $numThreads \
 -outputFullPred -create -overwriteExecDir \
 -examplesInSingleFile \
@@ -29,14 +29,14 @@ stanford-postagger-2010-05-26.jar \
 -testInputLists ${inputLists} \
 -execDir ${execDir} \
 -stagedParamsFile  ${stagedParamsFile} \
+-ngramModelFile output_file.arpa \
+-lengthCompensation 0 \
+-useStopNode 
 #-dmvModelParamsFile ${dmvModelParamsFile} \
--ngramModelFile atisLM/atis-all-train-3-gram.model.arpa \
 #-lengthPredictionModelFile ../datasets/atis/train/lengthPrediction.counts.linear-reg.model \
 #-lengthPredictionFeatureType counts \
 #-lengthPredictionStartIndex 2 \
--lengthCompensation 0 \
 #-posAtSurfaceLevel \
--useStopNode \
 #-interpolationFactor ${interpolationFactor} \
 #-useDependencies
 #-oracleReranker
